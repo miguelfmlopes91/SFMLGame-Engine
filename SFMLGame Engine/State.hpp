97d2f586dcb4ae4c8e35982c8158a606a6cd0190
miguellopes91 <miguelpvz@hotmail.com>
2018-05-17 21:57:26 +0100

    Added Game Loop and Main Scene
    Fix State.hpp on update, missing argument

--- SFMLGame Engine/State.hpp
+++ SFMLGame Engine/State.hpp
@@ -6,7 +6,7 @@ namespace Bardo
 		virtual void Init() = 0; //initialize the State
 
 		virtual void HandleInput() = 0; //handle all input on the particular state
-		virtual void Update() = 0;//update anything, variable, etc
+		virtual void Update(float dt) = 0;//update anything, variable, etc
 		virtual void Draw(float dt) = 0; //draws the action, Dt time difference between frames
 
 		virtual void Pause() =	0;
--- SFMLGame Engine/main.cpp
+++ SFMLGame Engine/main.cpp
@@ -1,10 +1,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "GameLoop.h"
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_WIDTH 1024
 
 int main()
 {
+	Bardo::GameLoop(800, 600, "Super Bardo Game");
+
 	return EXIT_SUCCESS;
 }
